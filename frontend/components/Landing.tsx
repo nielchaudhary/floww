519de8ddf13895fda972282f3e3fb0eae4237e66
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-05-05 03:21:50 +0530

    added landing components, button to be added

--- frontend/components/Landing.tsx
+++ frontend/components/Landing.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
-
+import { motion } from 'framer-motion';
+import { DotBackground } from './GridBackground';
 export const Landing = () => {
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen text-3xl fond-bold underline">
-            <h1>Landing</h1>
+
+        <div
+            
+        >
+            <DotBackground />
+
+          
         </div>
     )
 }
+
+
