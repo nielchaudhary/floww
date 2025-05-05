6cad8e30c45c686438e202c191c2289c7d171513
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-05-05 05:41:10 +0530

    init, logger

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import { Logger } from "./utils/logger";
+
+const app = express();
+const logger = new Logger('server');
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(8090, () => {
+    logger.info('FLOW SERVICES LIVE ON PORT 8090!!!');
+});
+
+
