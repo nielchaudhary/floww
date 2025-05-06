f0abb7ec94a59cb00b03c42a533cb8380a841d0b
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-05-07 04:37:33 +0530

    fix: updated prompt router in entry file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { Logger } from "./pkg/logger";
-
+import { promptRouter } from "./cmd/router/prompt";
 const app = express();
 const logger = new Logger('server');
-app.use(express.json());
 
 app.use(cors());
+app.use(express.json());
+
+app.use(promptRouter[0], promptRouter[1])
 
 app.listen(8090, () => {
     logger.info('FLOW SERVICES LIVE ON PORT 8090!!!');
