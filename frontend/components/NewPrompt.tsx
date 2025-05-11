0023d64457e8126c85413d85686b3bc121f28895
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-05-11 23:31:35 +0530

    fixed formatting, added React as import to remove errors

--- frontend/components/NewPrompt.tsx
+++ frontend/components/NewPrompt.tsx
@@ -1,16 +1,17 @@
-import { Spotlight } from "./Spotlight"
-import { Helmet } from "react-helmet"
-import { CreateNewPrompt } from "./PlaceHolderAndVanishInput"
+import { Spotlight } from './Spotlight';
+import { Helmet } from 'react-helmet';
+import { CreateNewPrompt } from './PlaceHolderAndVanishInput';
+import React from 'react';
 export const NewPrompt = () => {
-    return (
-        <>
-        <Helmet>
-            <title>floww | Create New Prompt</title>
-        </Helmet>
-        <div className="flex flex-col justify-center items-center h-screen w-screen bg-black relative overflow-hidden">
-            <Spotlight/>
-            <CreateNewPrompt/>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Helmet>
+        <title>floww | Create New Prompt</title>
+      </Helmet>
+      <div className="flex flex-col justify-center items-center h-screen w-screen bg-black relative overflow-hidden">
+        <Spotlight />
+        <CreateNewPrompt />
+      </div>
+    </>
+  );
+};
--- frontend/components/PaymentPage.tsx
+++ frontend/components/PaymentPage.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import { Spotlight } from './Spotlight';
 import { Helmet } from 'react-helmet';
+import React from 'react';
 
 export const PaymentPage = () => {
   return (
